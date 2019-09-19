f6996ad06814965bbc8f869f8b2b84b1178fdc92
mohlsson63 <momohlsson@gmail.com>
2019-09-19 09:05:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 window.alert("This site employs javascript");
+let cx = document.querySelector("b").getContext("2d");
+  cx.font = "28px Georgia";
+  cx.fillStyle = "Arial";
+  cx.fillText("EDUCATION", 40, 10);

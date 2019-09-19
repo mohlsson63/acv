ec710a01a225a48046176760e6a18156d26eabbe
mohlsson63 <momohlsson@gmail.com>
2019-09-19 11:46:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-window.alert("This site employs javascript");
+var v=window.alert("This site employs javascript");
+document.getQuerySelector("h1").addEventListener("mouseover",function(){alert(v);} );
 let cx = document.querySelector("b").getContext("2d");
   cx.font = "28px Georgia";
   cx.fillStyle = "Arial";

59243693061c674774f261697079b03a656dc5bb
mohlsson63 <momohlsson@gmail.com>
2019-09-19 11:58:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var v=window.alert("This site employs javascript");
-document.getQuerySelector("h3").addEventListener("click",function(){alert(v);} );
+document.getElementByTag("h3").addEventListener("click",function(){alert(v);} );
 let cx = document.querySelector("b").getContext("2d");
   cx.font = "28px Georgia";
   cx.fillStyle = "Arial";

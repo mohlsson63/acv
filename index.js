8c767139d0ec2f2c5fb337fef96ed831db403a6e
mohlsson63 <momohlsson@gmail.com>
2019-09-19 11:48:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var v=window.alert("This site employs javascript");
-document.getQuerySelector("h1").addEventListener("mouseover",function(){alert(v);} );
+document.getQuerySelector("h3").addEventListener("mouseover",function(){alert(v);} );
 let cx = document.querySelector("b").getContext("2d");
   cx.font = "28px Georgia";
   cx.fillStyle = "Arial";
